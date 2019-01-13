d6696c50dd93fe6ee5a04d25e7e1e92121eb8396
Ashley Levi <Ashplevi@gmail.com>
2019-01-13 10:15:10 -0700

    Updates

---
+++ precache-manifest.6280fde68a924fcb516b38dc5f709b1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2a09848d16ddd7146db5deb2206e218",
+    "url": "/movie-tracker/static/media/app_background.d2a09848.png"
+  },
+  {
+    "revision": "f5a723b953ab703507c1",
+    "url": "/movie-tracker/static/js/runtime~main.f5a723b9.js"
+  },
+  {
+    "revision": "f393aa278e8062ef95f0",
+    "url": "/movie-tracker/static/js/main.f393aa27.chunk.js"
+  },
+  {
+    "revision": "a956e73f7d5c1efc0d68",
+    "url": "/movie-tracker/static/js/1.a956e73f.chunk.js"
+  },
+  {
+    "revision": "f393aa278e8062ef95f0",
+    "url": "/movie-tracker/static/css/main.21df9e3a.chunk.css"
+  },
+  {
+    "revision": "0d3047e0a50dbe6b9ec0196f277ac4d9",
+    "url": "/movie-tracker/index.html"
+  }
+];
